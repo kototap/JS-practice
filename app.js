b20a41e6cbe44a138a00f94f425cf1f5c630fdba
Kototap <106796386+kototap@users.noreply.github.com>
2022-09-27 06:04:47 +0000

    複数の関数

--- app.js
+++ app.js
@@ -67,15 +67,30 @@
 
 
 // 引数と戻り値がある時の関数
-function double(number) {
-  // const result = number * 2;
-  // return result;
-  
-  // return number * 2; こっちだけでもOK
+// function double(number) {
+//   // const result = number * 2;
+//   // return result;
+
+//   // return number * 2; こっちだけでもOK
+// }
+
+// const a = double(10);
+// alert(a);
+
+// const b = double(8);
+// alert(b);
+
+
+// 引数の応用
+
+// 複数の引数
+function sum(a, b){
+  const result = a + b;
+  return result;
 }
 
-const a = double(10);
-alert(a);
+const x = sum(3, 5);
+alert(x);
 
-const b = double(8);
-alert(b);
+const y = sum(10, 20);
+alert(y);
