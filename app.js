9a5bdfeb65dcba69d3e91ea910993ea84a41fcb6
Kototap <106796386+kototap@users.noreply.github.com>
2022-09-27 05:13:13 +0000

    for...of文

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-for ( let i = 1; i < 10; i++) {
-  alert('hello');
+// for ( let i = 1; i < 10; i++) {
+//   alert('hello');
+// }
+
+
+// let number = 2;
+
+// while (number < 100) {
+//   alert(`${number}番`);
+//   number = number * 2;
+// }
+
+// let number = 1;
+
+// while (number <= 5) {
+//   alert(number);
+//   number++
+// }
+
+
+const fruits = ['りんご', 'みかん', 'バナナ'];
+
+for ( const fruit of fruits ) {
+  alert(fruit);
 }
