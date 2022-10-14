8a3c9c033a48642e18d9e417ddff500e294dd762
Kototap <106796386+kototap@users.noreply.github.com>
2022-10-14 06:38:40 +0000

    [Modify]input_event

--- input_event.js
+++ input_event.js
@@ -1,8 +1,8 @@
-const input = document.querySelector('input');
-input.addEventListener('input', function(event) {
-  const value = event.currentTarget.value;
-  console.log(`入力内容: ${value}`);
-});
+// const input = document.querySelector('input');
+// input.addEventListener('input', function(event) {
+//   const value = event.currentTarget.value;
+//   console.log(`入力内容: ${value}`);
+// });
 
 const radios = document.querySelectorAll('.radio');
 for (let radio of radios) {
